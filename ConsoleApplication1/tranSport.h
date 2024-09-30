2539fd7f6474146aeec0b299e0317de133f1d14c
qjy123123 <100759977+qjy123123@users.noreply.github.com>
2024-09-30 10:11:37 +0800

    Update tranSport.h

--- ConsoleApplication1/tranSport.h
+++ ConsoleApplication1/tranSport.h
@@ -20,19 +20,19 @@ public:
     unsigned char *getInputReport(int lenth);
     //读取设备的反馈信息 需要传入一个unsigned char 数组，若成功写入数据数组会被赋值ack ok，若设备被启动，按下按键，下标为9的字节中存放按键标号，下标为10的字节中存放按键状态，0x00按键抬起，0x01按键按下
     int read(unsigned char *data,unsigned long lenth);
-    //向设备下发信息
+    //向设备下发信息，data:装有数据的unsigned char数组，lenth：数组长度
     int write(unsigned char *data,unsigned long lenth);
     //将enumerate返回的设备信息列表销毁
     void freeEnumerate(hid_device_info *devs);
-    //获取设备信息列表
+    //获取设备信息列表，vid：设备vid  pid：设备vid
     hid_device_info *enumerate(int vid, int pid);
     //设置设备屏幕亮度 1-100
     int setBrightness(int percent);
     //设置设备屏幕的背景图，buffer：放有BGR数据的unsigned char数组地址，size：数组大小
     int setBackgroundImg(unsigned char *buffer,int size);
-    //设置设备某个按键的图标
+    //设置设备某个按键的图标，path：图片路径，key：按键标号
     int setKeyImg(std::string path,int key);
-    //设置设备某个按键的图标,通过传数据
+    //设置设备某个按键的图标,buffer，装有图片信息的unsigned char 数组，key：按键标号 width： 图片长度 height：图片高度
     int setKeyImgdata(unsigned char* buffer, int key, int width, int height);
     //清空某个按键的图标 1-15
     int keyClear(int i);
